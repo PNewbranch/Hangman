ef97322dc9a2a1d75f20f6cda292a8672904cd50
P N <p.newbranch@outlook.com>
2019-10-29 11:31:34 +0100

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+
+        private static string InitiateGame() //returns a random word
+        {
+        string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
+        foreach (string value in wordList) { Console.WriteLine(value); }  
+        Random randomNo = new Random();
+        //Console.WriteLine(wordList [randomNo.Next(0,9)]);
+        string seacretWord = wordList[randomNo.Next(0, 9)];
+        //Console.WriteLine(seacretWord);
+            return seacretWord;
+        }
+
+        private static string ReplaceAChar(string cleanedWord) 
+        {
+            return "dummy";
+        }
+        
+
+        static void Main(string[] args)
+        {
+            string correctWord = InitiateGame();
+            string celandedWord = ReplaceAChar(correctWord);
+
+
+
+        } //main
+    } //program
+} //namespace
