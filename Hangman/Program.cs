24813c8f75be68c9bf01465f40b594e62947d308
P N <p.newbranch@outlook.com>
2019-10-29 15:54:21 +0100

    Sitter fast på "replace ti -" (blanka ut ordet).

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -5,7 +5,7 @@ namespace Hangman
     class Program
     {
 
-        private static string InitiateGame() //returns a random word
+        private static string GetARandomWord() //returns a random word
         {
         string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
         foreach (string value in wordList) { Console.WriteLine(value); }  
@@ -16,17 +16,40 @@ namespace Hangman
             return seacretWord;
         }
 
-        private static string ReplaceAChar(string cleanedWord) 
+        private static string ReplaceAllChar(string textToClean) 
         {
-            return "dummy";
+
+            string Cstring;
+
+            for (int i = 0; i < textToClean.Length; i--)
+            {
+                //str = str.Replace("quick", "brown"); //byt ut en del texter
+                Cstring = Cstring.Replace(char.i,"-");
+            }
+            return "----";
         }
-        
+
+        private static int AskForInput() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
+        {
+            Console.Write("Ange en bokstav: ");
+            return 'a';
+        }
+
+        private static string CheckWord (string cleanedWord)
+        {
+            return "jämför ord";
+        }
+
 
         static void Main(string[] args)
         {
-            string correctWord = InitiateGame();
-            string celandedWord = ReplaceAChar(correctWord);
 
+            string correctWord = GetARandomWord();
+            string coveredWord = ReplaceAllChar(correctWord);
+
+
+
+            Console.WriteLine(coveredWord);
 
 
         } //main
