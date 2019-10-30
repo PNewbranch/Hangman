f9745191d287126f9c0cd4bcf3862d2f06e2dfa0
P N <p.newbranch@outlook.com>
2019-10-30 13:13:00 +0100

    Created a "covered word" e.g. "----".

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,51 +7,52 @@ namespace Hangman
 
         private static string GetARandomWord() //returns a random word
         {
-        string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
-        foreach (string value in wordList) { Console.WriteLine(value); }  
-        Random randomNo = new Random();
-        //Console.WriteLine(wordList [randomNo.Next(0,9)]);
-        string seacretWord = wordList[randomNo.Next(0, 9)];
-        //Console.WriteLine(seacretWord);
-            return seacretWord;
+            string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
+            foreach (string value in wordList) { Console.WriteLine(value); }  
+            Random randomNo = new Random();
+            Console.WriteLine(wordList [randomNo.Next(0,9)]);
+            string secretWord = wordList[randomNo.Next(0,9)];
+            Console.WriteLine(secretWord);
+            return secretWord;
         }
 
-        private static string ReplaceAllChar(string textToClean) 
-        {
 
-            string Cstring;
 
-            for (int i = 0; i < textToClean.Length; i--)
-            {
-                //str = str.Replace("quick", "brown"); //byt ut en del texter
-                Cstring = Cstring.Replace(char.i,"-");
-            }
-            return "----";
-        }
+        //private static int AskForInput() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
+        //{
+        //    Console.Write("Ange en bokstav: ");
+        //    return 'a';
+        //}
 
-        private static int AskForInput() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
-        {
-            Console.Write("Ange en bokstav: ");
-            return 'a';
-        }
-
-        private static string CheckWord (string cleanedWord)
-        {
-            return "jämför ord";
-        }
+        //private static string CheckWord (string cleanedWord)
+        //{
+        //    return "jämför ord";
+        //}
 
 
         static void Main(string[] args)
         {
 
-            string correctWord = GetARandomWord();
-            string coveredWord = ReplaceAllChar(correctWord);
+            string correctWord = GetARandomWord(); //get a radom word
+            Console.WriteLine(correctWord);
 
-
-
+            char[] coveredWord = new char[correctWord.Length];  //create a array with character
+            for (int i=0; i < (correctWord.Length); i++)        //fill it with -
+            {
+                coveredWord[i] = '-';
+            }
+            //coveredWord[0] = 'd';
             Console.WriteLine(coveredWord);
 
 
+            ////char coveredWord char[];
+            ////string coveredWord = ReplaceAllChar(correctWord);
+
+            //Console.WriteLine(coveredWord);
+
         } //main
+
+
+
     } //program
 } //namespace
