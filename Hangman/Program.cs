dedaaa6889589d498d9d473314fbdf17cbd26e4b
P N <p.newbranch@outlook.com>
2019-10-30 14:40:20 +0100

    Väljer ut random ord och skapar wn ARRAY av CHAR som innehåller motsvarande ord där varje bokstav är dold (-).

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -8,47 +8,42 @@ namespace Hangman
         private static string GetARandomWord() //returns a random word
         {
             string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
-            foreach (string value in wordList) { Console.WriteLine(value); }  
-            Random randomNo = new Random();
-            Console.WriteLine(wordList [randomNo.Next(0,9)]);
-            string secretWord = wordList[randomNo.Next(0,9)];
-            Console.WriteLine(secretWord);
-            return secretWord;
+            foreach (string value in wordList) 
+            { 
+                Console.WriteLine(value); 
+            }  
+            Random randomNo = new Random(); //get a random number
+            return wordList[randomNo.Next(0, 9)]; //use random number to pick a word
         }
 
+        private static char AskForACharacter() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
+        {
+            Console.WriteLine("Ange en bokstav: ");
+            return Console.ReadKey().KeyChar; //NOTERA!!!!!! - glöm inte punkten som talar om vad som skall retuneras
+        }
 
-
-        //private static int AskForInput() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
-        //{
-        //    Console.Write("Ange en bokstav: ");
-        //    return 'a';
-        //}
-
-        //private static string CheckWord (string cleanedWord)
-        //{
-        //    return "jämför ord";
-        //}
+        private static string CheckWord(string cleanedWord)
+        {
+            return "jämför ord";
+        }
 
 
         static void Main(string[] args)
         {
 
-            string correctWord = GetARandomWord(); //get a radom word
-            Console.WriteLine(correctWord);
+            string correctWord = GetARandomWord(); //get a radom word from list
+            Console.WriteLine("Random ord: \n" + correctWord);
+            
 
-            char[] coveredWord = new char[correctWord.Length];  //create a array with character
-            for (int i=0; i < (correctWord.Length); i++)        //fill it with -
+            char[] coveredWord = new char[correctWord.Length]; //create a array with character
+            for (int i=0; i < (correctWord.Length); i++) //fill it with -
             {
                 coveredWord[i] = '-';
             }
-            //coveredWord[0] = 'd';
-            Console.WriteLine(coveredWord);
+            Console.WriteLine("Slumpvist valda ordet dolt: "); //visar det dolda ordet 
+            Console.WriteLine(coveredWord); //visar det dolda ordet 
 
-
-            ////char coveredWord char[];
-            ////string coveredWord = ReplaceAllChar(correctWord);
-
-            //Console.WriteLine(coveredWord);
+            Console.WriteLine(AskForACharacter());
 
         } //main
 
