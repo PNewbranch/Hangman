904d8815b69b0b9ea4b920126e0d56af5c9e177b
P N <p.newbranch@outlook.com>
2019-10-31 09:31:24 +0100

    Now with a functional menu.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -5,13 +5,16 @@ namespace Hangman
     class Program
     {
 
-        private static string GetARandomWord() //returns a random word
+        private static string GetARandomWord() //skapar en array med 10 ord och returnerar ett random word
         {
             string[] wordList = new string[] {"solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
+            Console.WriteLine("Dessa ord har vi i listan:");
             foreach (string value in wordList) 
             { 
                 Console.WriteLine(value); 
             }  
+
+            //bryt eventuellt ut detta i egen metod
             Random randomNo = new Random(); //get a random number
             return wordList[randomNo.Next(0, 9)]; //use random number to pick a word
         }
@@ -21,11 +24,21 @@ namespace Hangman
             char[] coveredWord = new char[wordToCover.Length]; //create a array with character
             for (int i = 0; i < (wordToCover.Length); i++) //fill it with -
             {
-                coveredWord[i] = 's';
+                coveredWord[i] = '-';
             }
-            Console.WriteLine("Slumpvist valda ordet dolt: "); //visar det dolda ordet 
+            Console.Write("Ordet i dold version blir: "); //visar det dolda ordet 
             Console.WriteLine(coveredWord); //visar det dolda ordet 
+            Console.WriteLine();
             return coveredWord;
+        }
+
+        private static void ShowUserChoiseMeny()
+        {
+            Console.WriteLine("Du har tio unika försök på dig att gissa ordet.");
+            Console.WriteLine("Ange 1 för att gissa på bokstav.");
+            Console.WriteLine("Ange 2 för att gissa på ord.");
+            Console.WriteLine("Ange 9 för att avsluta.\n");
+            Console.Write("Gör ditt val: ");
         }
 
         private static char AskForACharacter() //(outparametern) RETURN-värdet skrivs FÖRE metodnamnet - return= DOUBLE
@@ -39,24 +52,20 @@ namespace Hangman
             return "jämför ord";
         }
 
-
         static void Main(string[] args)
         {
 
             string correctWord = GetARandomWord(); //get a radom word from list
-            Console.WriteLine("Random ord: \n" + correctWord);
+            Console.WriteLine("\nDet slumpvisa valda ordet: " + correctWord);
 
-            char[] coveredWord = new char[correctWord.Length];
-            coveredWord = CreateACoveredWord(correctWord);
+            char[] coveredWord = new char[correctWord.Length]; //skapa en kopia av det valda ordet
+            coveredWord = CreateACoveredWord(correctWord);     //strecka ut alla bokstäver
 
-           bool uppRunning = true;
-           while (uppRunning)
+            bool uppRunning = true;
+            while (uppRunning)
             {
-                Console.WriteLine("Ange 1 för att gissa på bokstav.");
-                Console.WriteLine("Ange 2 för att gissa på ord.");
-                Console.WriteLine("Ange 9 för att avsluta.");
+                ShowUserChoiseMeny();
                 char usersLoopChoise = Console.ReadKey().KeyChar;
-
                 switch (usersLoopChoise)
                 {
                     case '9':
@@ -69,24 +78,11 @@ namespace Hangman
                     case '2':
                         Console.WriteLine("Vi testar på ordet!");
                         break;
-                }
+                } //switch
 
-            }
-
-
-
-
-
-            char characterFromUser = AskForACharacter();
-            Console.WriteLine(characterFromUser); 
-            
-            Console.WriteLine(AskForACharacter());
-
-
+            } //while
 
         } //main
-
-
 
     } //program
 } //namespace
