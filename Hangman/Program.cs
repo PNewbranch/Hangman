59c9d1c94e4df4cd07f042204913f65f679dbf55
PNewbranch <p.newbranch@outlook.com>
2019-11-02 17:04:36 +0100

    Almost there - double case1 Grattis på rätt via bokstavsgissning.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,32 +6,11 @@ namespace Hangman
 {
     class Program
     {
-
-        //private static string[] CreateAListOfWords() //finns ingen anledning att skapa en egen metod för detta
-        //{
-        //    string[] wordList = new string[] { "solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
-        //    Console.WriteLine("Dessa ord har vi i listan:");
-        //    foreach (string value in wordList)
-        //    {
-        //        Console.WriteLine(value);
-        //    }
-        //        return wordList;
-        //} //finns ingen anledning att skapa en egen metod för detta
-
-
         private static string GetARandomWord(string[] listOfWords) //skapar en array med 10 ord och returnerar ett random word
         {
-            //string[] wordList = new string[] { "solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
-            //Console.WriteLine("Dessa ord har vi i listan:");
-            //foreach (string value in wordList)
-            //{
-            //    Console.WriteLine(value);
-            //}
-
-            //bryt eventuellt ut detta i egen metod
             Random randomNo = new Random(); //get a random number
             string randomWord = listOfWords[randomNo.Next(0, 9)];
-            Console.WriteLine("Det utvalda ordet är :" + randomWord);
+            //Console.WriteLine("Det utvalda ordet är :" + randomWord);
             return randomWord;  //use random number to pick a word
         }
 
@@ -47,18 +26,7 @@ namespace Hangman
             return coveredWord;
         }
 
-        //public static void ShowUserChoiseMeny()
-        //{
-        //    Console.Clear();
-        //    Console.WriteLine("HANGMAN");
-        //    Console.Write("Gissa ordet: ");
-        //    Console.WriteLine(coveredWord);
-        //    Console.WriteLine("Du har 10 unika gissningar, antalet försök kvar: " + counter);
-        //    Console.WriteLine("Hitintills använda bokstäver: " + usedCharacters);
-        //    Console.WriteLine("\nDina val:\n1 = testa bokstav\n2 = testa ord\n9 = ge upp");
-        //}
-
-        private static char AskForACharacter(string infoToUSer)   //metoden skriver ut den text modulen anropas med
+        private static char AskForACharacter(string infoToUSer) //metoden skriver ut den text modulen anropas med
         {
             Console.Write(infoToUSer);
             return Console.ReadKey().KeyChar;               //NOTERA!!!!!! - glöm inte punkten som talar om typen/vad som skall retuneras
@@ -70,100 +38,107 @@ namespace Hangman
             return Console.ReadLine();
         }
 
-        private static bool CheckAWord(string inputWord)
-        {
-            Console.Write("\n Ange ord du vill testa: ");
-            string wordToTest = Console.ReadLine();
-            Console.WriteLine(wordToTest);
-            return true;
-        }
-
-
 
         static void Main(string[] args)
         {
-
- 
+            //initiate definition of data sources
             string[] wordList = new string[] { "solros", "mask", "orm", "finansinspektionen", "kråka", "varulv", "gädda", "rosmarin", "avvikelse", "munk" };
-            string correctWord = GetARandomWord(wordList);      //get a radom word from list
-            char[] coveredWord = new char[correctWord.Length];  //skapa en char-array med lika många bokstäver om originalordet
-            coveredWord = CreateACoveredWord(correctWord);      //kasta in ordet och få tillbaka den streckade varianten
-            StringBuilder usedCharacters = new StringBuilder();
-                
-            //ShowUserChoiseMeny();
 
-            //while ( )
-            //{
-
-            //}
-
-            int counter = 10;
-            //bool uppRunning = true;
-            while (counter > 0)
+            bool keepRunning = true;
+            while (keepRunning)
             {
-                Console.Clear();
-                Console.WriteLine(correctWord);
-                Console.WriteLine("HANGMAN");
-                Console.Write("Gissa ordet: ");
-                Console.WriteLine(coveredWord);
-                Console.WriteLine("Du har 10 unika gissningar, antalet försök kvar: " + counter);
-                Console.WriteLine("Hitintills testade bokstäver: " + usedCharacters);
-                Console.WriteLine("\nDina val:\n1 = testa bokstav\n2 = testa ord\n9 = ge upp");
+                //initiate game round depended data
+                string correctWord = GetARandomWord(wordList);      //get a radom word from list
+                char[] coveredWord = new char[correctWord.Length];  //skapa en char-array med lika många bokstäver om originalordet
+                coveredWord = CreateACoveredWord(correctWord);      //kasta in ordet och få tillbaka den streckade varianten
+                int NoOfCharToAssign = coveredWord.Length;
+                StringBuilder usedCharacters = new StringBuilder();
 
-                //ShowUserChoiseMeny();
-                char usersLoopChoise = AskForACharacter("\nAnge ditt val: "); //kastar in testen som frågar efter char
-                switch (usersLoopChoise)
+                char playOrQuit = AskForACharacter("\nVälj valfri tangent för spela HANGMAN eller 9 för att avsluta: ");
+                switch (playOrQuit)
                 {
-                    case '9':
-                        AskForACharacter("\nDu har valt att ge upp!");
-                        //uppRunning = false; //deaktiverar yttre loopen
-                        counter = 0;
-                        break;
-                    case '1':
-                        char charFromUser = AskForACharacter(" Ange en bokstav: ");
-                        usedCharacters.Append(charFromUser);
+                case '9':
+                    keepRunning = false;
+                    break;
+                default :
+                    int counter = 10;
+                    while (counter > 0)
+                    {
+                        //lägg ev i egen metod med inparametrar
+                        Console.Clear();
+                        Console.WriteLine("Slumpat ord: " + correctWord);
+                        Console.WriteLine("HANGMAN");
+                        Console.Write("Gissa ordet: ");
+                        Console.WriteLine(coveredWord);
+                        Console.WriteLine("Du har 10 unika gissningar, antalet försök kvar: " + counter);
+                        Console.WriteLine("Hitintills testade bokstäver: " + usedCharacters);
+                        Console.WriteLine("\nDina val:\n1 = testa bokstav\n2 = testa ord\n9 = ge upp");
 
-                        int startPos = 0;
-                        while (startPos > -1 )
+                        char usersLoopChoise = AskForACharacter("\nAnge ditt val: "); 
+                        switch (usersLoopChoise)
                         {
-                            int existsInPos = correctWord.IndexOf(charFromUser, startPos); //om bokstav finns, ta reda på dess placering
-                            if (existsInPos > -1)
-                            {
-                                coveredWord[existsInPos] = charFromUser;
-                                //Console.WriteLine("Placering" + existsInPos);
-                                //System.Threading.Thread.Sleep(1000);  
-                                startPos = existsInPos + 1;
-                            }
-                            else
-                            {
-                                startPos = -1;
-                            }
-                        }
-                        counter--;
-                        break;
-                    case '2':
-                        string wordFromUser = AskForAString(" Ange ett ord: ");
-                        if (wordFromUser == correctWord) 
-                        {
-                            Console.Write("Grattis du har gissat rätt ord: ");
-                            for (int i=0; i<correctWord.Length; i++)
-                            {
-                                coveredWord[i] = correctWord[i];
-                            }
-                            Console.WriteLine(coveredWord);
-                            System.Threading.Thread.Sleep(3000);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ordet var fel, försök igen");
-                            counter--;
-                        }
-                        break;
-                } //switch
+                            case '9':
+                                Console.WriteLine("\nDu har valt att ge upp!");
+                                System.Threading.Thread.Sleep(2000);
+                                counter = 0;
+                                break;
+                            case '1':
+                                char charFromUser = AskForACharacter(" Ange en bokstav: ");
+                                usedCharacters.Append(charFromUser);
 
-            } //while
+                                int startPos = 0;
+                                while (startPos > -1 )
+                                {
+                                    int existsInPos = correctWord.IndexOf(charFromUser, startPos); //om bokstav finns, ta reda på dess placering
+                                    if (existsInPos > -1)
+                                    {
+                                        coveredWord[existsInPos] = charFromUser;
+                                        startPos = existsInPos + 1;
+                                            NoOfCharToAssign--;
+                                    }
+                                    else
+                                    {
+                                        startPos = -1;
+                                    }
 
+                                    if (NoOfCharToAssign == 0)
+                                    {
+                                            for (int i = 0; i < correctWord.Length; i++)
+                                            {
+                                                coveredWord[i] = correctWord[i];
+                                            }
+                                            Console.Write("\nCase 1 - Grattis du har gissat rätt ord: ");
+                                            Console.WriteLine(coveredWord);
+                                            System.Threading.Thread.Sleep(500);
+                                            counter = -99;
+                                     }
+                                }
+                                counter--;
+                                break;
+                            case '2':
+                                string wordFromUser = AskForAString(" Ange ett ord: ");
+                                if (wordFromUser == correctWord) 
+                                {
+                                    for (int i=0; i<correctWord.Length; i++)
+                                    {
+                                        coveredWord[i] = correctWord[i];
+                                    }
+                                    Console.Write("\nCase 2 - Grattis du har gissat rätt ord: ");
+                                    Console.WriteLine(coveredWord);
+                                    System.Threading.Thread.Sleep(500);
+                                    counter = 0;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ordet var fel, försök igen");
+                                    counter--;
+                                }
+                                break;
+                        } //switch
+                    } //while counter
+                    break;
+                }
+            } //while keepRunning
         } //main
-
     } //program
 } //namespace
